027393838ead59ca6299f334f3e300c423205538
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2023-05-16 18:46:55 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import scratchattach as scratch3
 session = scratch3.login("Iegend-", "kitloin!@")
+import random
 from time import sleep
 conn = session.connect_cloud("846329992")
 while True:
-    conn.set_var("TO_HOST", "519631822125254961475996171213121016191113")
+    conn.set_var("TO_HOST", "519631822125254961475996171213121016191113." + random.randint(100000, 999999))
