e374d904bea9dcfb52c2a3439cd181503cbc33d3
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2023-05-16 19:01:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ import random
 from time import sleep
 conn = session.connect_cloud("846329992")
 while True:
-    conn.set_var("TO_HOST", "519631822125254961475996171213121016191113." + random.randint(10000, 99999))
+    rarbingk = random.randint(10000, 99999)
+    conn.set_var("TO_HOST", "519631822125254961475996171213121016191113." + rarbingk)
