9073037efb65c242816f607f2bf2af82847a1c66
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2023-05-16 18:33:03 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ session = scratch3.login("Iegend-", "kitloin!@")
 from time import sleep
 conn = session.connect_cloud("846329992")
 while True:
-    conn.set_var("TO_HOST", "519631822125254961475996171213121016191113.509300")
+    conn.set_var("TO_HOST", "519631822125254961475996171213121016191113")
