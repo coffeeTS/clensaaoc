3b5b1a50a25c41d078df26552e3cad78147a87a9
Sonik <69720319+SuperS0n1k@users.noreply.github.com>
2023-05-20 17:05:53 +0100

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-import scratchattach as scratch3
-session = scratch3.login("Iegend-", "kitloin!@")
-import random
-from time import sleep
-conn = session.connect_cloud("846329992")
-while True:
-    rarbingk = str(random.randint(10000, 99999)) + "0"
-    conn.set_var("TO_HOST", "519631822125254961475996171213121016191113." + rarbingk)
